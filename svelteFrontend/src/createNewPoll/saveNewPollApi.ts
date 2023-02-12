07a9334c29096fa28e3e1b04e1cf55aa1bccd25c
Your Name <timongisler@icloud.com>
2023-02-12 18:06:52 +0100

    added pocketbase sdk

--- svelteFrontend/src/createNewPoll/saveNewPollApi.ts
+++ svelteFrontend/src/createNewPoll/saveNewPollApi.ts
@@ -1,5 +1,17 @@
 import type { Vote as CreateVoteDto } from "./CreatePollDto";
+import PocketBase from "pocketbase";
 
-export function saveVote(voteToSave:CreateVoteDto):string{
-  return "someUniqueId"
+
+export async function saveVote(voteToSave: CreateVoteDto): Promise<string> {
+
+  const pb = new PocketBase("http://127.0.0.1:8090");
+
+  // example create data
+  const data = {
+    name: "test",
+  };
+
+  const record = await pb.collection("poll").create(data);
+
+  return "someUniqueId";
 }
